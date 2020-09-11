0dbd1cdf0bb39c5f5a2774b144ad632192530bec
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 19:38:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index| 
     puts #{index +1}. #{dwarf}
 end
+puts #{index +1}. #{dwarf}
 end
 
 def summon_captain_planet# code an argument here
