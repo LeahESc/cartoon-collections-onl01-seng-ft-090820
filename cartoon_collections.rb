7839bd0322e87833edf55f99c34b8178d8aa2075
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 19:55:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.detect {# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect {|fromage| fromage == "cheddar" || "gouda" || "camembert"}
+  puts 
 end
