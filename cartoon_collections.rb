0c1db425bb51f805b12f9c2c124a6d4f98fc81b5
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 20:10:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(array)
   if array.include? ("cheddar") ||  ("gouda") || ("camembert")
-     return 
+     array.find {|frommage| fromage == "cheddar" || "gouda" || "camembert" 
   else
     puts "No cheese, here."
   end
